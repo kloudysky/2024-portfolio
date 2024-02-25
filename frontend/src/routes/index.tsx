35913592cc93b8200e6e6677c494034838b6e548
kloudysky <11273182+kloudysky@users.noreply.github.com>
2024-02-24 22:44:41 -0600

    Added navigation component

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Navigation } from "~/components/navigation/navigation";
 
 export default component$(() => {
   return (
     <>
+      <Navigation />
       <h1>Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
@@ -15,11 +17,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Welcome to Koud's portfolio",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "Kloud's Portfolio",
+      content: "Kloud's 2024 portfolio using Qwik & Flask",
     },
   ],
 };
